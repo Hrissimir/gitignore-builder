278e940223d1c0468ae0f8ccffd24d301e63f12b
Hrissimir <hrisimir.dakov@gmail.com>
2022-09-29 17:37:28 +0300

    fixed warnings in __about__.py

--- src/gitignore_builder/__about__.py
+++ src/gitignore_builder/__about__.py
@@ -1,4 +1,5 @@
+# pylint: disable=missing-module-docstring,invalid-name
 # SPDX-FileCopyrightText: 2022-present Hrissimir <hrisimir.dakov@gmail.com>
 #
 # SPDX-License-Identifier: MIT
-__version__ = '0.1.1'
+__version__ = '0.1.1'  # pragma: no cover
