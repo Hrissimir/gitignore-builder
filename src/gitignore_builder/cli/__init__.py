781ca3f5f1887468f45965ff9e6f26e6bf02a24c
Hrissimir <hrisimir.dakov@gmail.com>
2022-10-04 09:19:30 +0300

    Refactored gitignore_builder.cli.__init__.py

--- src/gitignore_builder/cli/__init__.py
+++ src/gitignore_builder/cli/__init__.py
@@ -7,6 +7,7 @@ import click
 
 from gitignore_builder import builder
 from gitignore_builder import datamodel
+from ..__about__ import __version__  # pylint: disable=relative-beyond-top-level
 
 CONTEXT_SETTINGS = {
     "help_option_names": ["-h", "--help"],
@@ -18,42 +19,34 @@ CONTEXT_SETTINGS = {
 datamodel.init()
 
 
-def print_config(ctx, param, value):  # pylint: disable=unused-argument
+def show_files(ctx, _, value):
     if not value or ctx.resilient_parsing:
         return
-    click.echo(f"Recipes file: '{datamodel.get_recipes_file()}'")
-    click.echo(f"Templates file: '{datamodel.get_templates_file()}'")
+    click.echo(f"recipes file: {gitignore_builder.datamodel.get_recipes_file()}")
+    click.echo(f"templates file: {gitignore_builder.datamodel.get_templates_file()}")
     ctx.exit()
 
 
 @click.command(context_settings=CONTEXT_SETTINGS, no_args_is_help=True)
-@click.option("-c", "--config",
-              is_flag=True,
-              callback=print_config,
-              expose_value=False,
-              is_eager=True,
-              help="Print the location of app config files.")
-@click.argument("recipe",
-                type=click.Choice(datamodel.get_recipe_names()))
-@click.argument("output",
-                type=click.File("w"),
-                default="-")
+@click.version_option(version=__version__, prog_name="gitignore-builder")
+@click.option(
+    "--files",
+    is_flag=True,
+    callback=show_files,
+    expose_value=False,
+    is_eager=True,
+    help="Show paths to app data-files and exit."
+)
+@click.argument("recipe", type=click.Choice(datamodel.get_recipe_names()))
+@click.argument("output", type=click.File("w"), default="-")
 def gitignore_builder(recipe, output):
-    """Generate .gitignore contents from recipe and write them to the output."""
+    """Build .gitignore contents from recipe URLs and write result to output."""
 
     click.echo(f"Building .gitignore contents using recipe: '{recipe}' ...")
-
-    lines = []
-
-    urls = list(datamodel.get_recipe_urls(recipe))
-    with click.progressbar(urls) as recipe_urls:
-        for url in recipe_urls:
-            title = f"source: {url}"
-            builder.append_url(lines, url, title)
-
-    text = "\n".join(lines)
+    urls = datamodel.get_recipe_urls(recipe)
+    text = builder.build_gitignore_contents(urls)
     click.echo("...done!")
 
-    click.echo(f"Writing the resulting .gitignore contents to: '{output}' ...")
+    click.echo(f"Writing the result to: '{output}' ...")
     click.echo(text, file=output)
-    click.echo("...done!")
+    click.echo("...all done!")
